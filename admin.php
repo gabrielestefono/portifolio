08544bfe7d0285305ae6500adff3f8659b297d1f
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-05 09:07:55 -0300

    Recomeçando o portfólio - Novamente (Socorro)

--- admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("./helper/url.php");
-include_once("./templates/header.php")
-?>
-<?php
-include_once("./templates/footer.php")
-?>
--- templates/navbar.php
+++
@@ -1,6 +0,0 @@
-<?php include_once("./helper/url.php")?>
-<header class="header">
-    <navbar class="nav">
-        <a href="<?=$BASE_URL?>"><div class="nav-logo"></div></a>
-    </navbar>
-</header>
