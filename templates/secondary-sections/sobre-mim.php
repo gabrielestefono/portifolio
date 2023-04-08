9dd59ed15139d9902deff0f66b342eef097a57cd
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-08 09:59:53 -0300

    About - Mostrando Prévia ao usuário

--- templates/secondary-sections/sobre-mim.php
+++ templates/secondary-sections/sobre-mim.php
@@ -8,13 +8,18 @@
             <th class="adminabout__table--thead_title">Editar</th>
             <th class="adminabout__table--thead_title">Apagar</th>
         </thead>
+        <?php foreach($paragrafos as $paragrafo):?>
         <tr class="adminabout__table--row">
-            <td class="adminabout__table--row_id adminabout__table--row_item">1</td>
-            <td class="adminabout__table--row_text adminabout__table--row_item">jkasd kasasdhjkl  dasd a das</td>
+            <?php
+                $paragraphpreview = substr($paragrafo['paragrafo'],0,30);
+            ?>
+            <td class="adminabout__table--row_id adminabout__table--row_item"><?=$paragrafo['id']?></td>
+            <td class="adminabout__table--row_text adminabout__table--row_item"><?=$paragraphpreview?></td>
             <td class="adminabout__table--row_see adminabout__table--row_item"><a href="admin.php?pag=rsobre-mim.php&see=1&type=2" class="adminabout__table--row_item-link"><i class="fa-regular fa-eye"></i></a></td>
             <td class="adminabout__table--row_edit adminabout__table--row_item"><a href="admin.php?pag=usobre-mim.php&see=1&type=2" class="adminabout__table--row_item-link"><i class="fa-regular fa-pen-to-square"></i></a></td>
             <td class="adminabout__table--row_delete adminabout__table--row_item"><a href="admin.php?pag=dsobre-mim.php&see=1&type=2" class="adminabout__table--row_item-link"><i class="fa-regular fa-trash-can"></i></a></td>
         </tr>
+        <?php endforeach?>
     </table>
     <a href="admin.php?pag=csobre-mim.php&type=2" class="adminabout__button">Criar novo</a>
 </section>
